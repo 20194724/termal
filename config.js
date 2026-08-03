0578266e668e0608af3d73ae63f39cf4c596c592
LaCasaDeLasPuertas <gonzalovidalon002@gmail.com>
2026-08-03 11:27:15 -0500

    Conectar ERP v2 al backend real de desarrollo

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  * Sigue las instrucciones de README.md y reemplaza el texto entre comillas.
  */
 const CONFIG = Object.freeze({
-  API_URL: "https://script.google.com/macros/s/AKfycbxPS6zPI19AMWzCEYmrtipWT3lr_ZseWZk4Zikkz027AxrS3IrdbrqE633x-zc5hJcz2A/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbzPgez9Rqg4Cj00JZxQjSMZkv_NGLLJ9EQcnpgmI1wlBDCNQHHcTKsK6Ug5Egizx6VF4g/exec",
   SYNC_INTERVAL_MS: 30000,
   REQUEST_TIMEOUT_MS: 20000,
   DEMO_STORAGE_KEY: "termal_erp_demo_v3"
